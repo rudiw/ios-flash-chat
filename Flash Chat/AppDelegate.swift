48b8d18c287dd1a349884ec84ab9df30ea1f2763
rudiwijaya <rudi.wijaya@mitrais.com>
2019-02-25 08:55:37 +0700

    skeleton with fire with firebase and old bunlde id (I-Am-Poor)

--- Flash Chat/AppDelegate.swift
+++ Flash Chat/AppDelegate.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import Firebase
 
 
 @UIApplicationMain
@@ -18,7 +19,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         
         //TODO: Initialise and Configure your Firebase here:
-        
+        FirebaseApp.configure()
         
         return true
     }
